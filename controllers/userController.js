a7269a45b33fa928593bfa2bfdcf8b0a7256e0ca
Dev Nome <63138189+NumanKhan0103@users.noreply.github.com>
2025-01-27 22:15:44 +0500

    update port in the .env and crate getuser function in the userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,16 @@
+const User = require('./models/userModel.js');
+
+
+// get all users
+
+const getUsers = async (req, res) => {
+
+    try {
+
+        const users = await User.find();
+        res.status(200).json(users);
+
+    }catch (err) {
+        res.status(500).json({message: err.message});
+    }
+};
--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const app = express();
 app.use(express.json());
 
 // routes 
-app.use('/api/users', require('./routes/userRoutes'));
+// app.use('/api/users', require('./routes/userRoutes'));
 
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
